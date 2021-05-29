ff711676aee31ddb4a0ec3d82d18c6d6f64433ee
Edem <ziddah.edem@gmail.com>
2021-05-29 22:42:59 +0000

    :tada: initial commit

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ window.addEventListener("scroll", () => {
 });
 
 // Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+    const el = document.getElementById(id);
+    const fixNav = navBar.classList.contains("fix-nav");
+    let position = el.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    navBar.classList.remove("show");
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+  });
+});
 
 // Toggle Menu
 const menu = document.querySelector(".menu");
@@ -93,7 +113,7 @@ new TypeIt("#type1", {
   .type("Designer", { delay: 400 })
   .pause(500)
   .delete(9)
-  .type("developer", { delay: 400 })
+  .type("Developer", { delay: 400 })
   .pause(500)
   .delete(9)
   .go();
@@ -106,17 +126,19 @@ new TypeIt("#type2", {
   .type("Designer", { delay: 400 })
   .pause(500)
   .delete(9)
-  .type("developer", { delay: 400 })
+  .type("Developer", { delay: 400 })
   .pause(500)
   .delete(9)
   .go();
 
 gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
 gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 0.8, x: 20 });
-gsap.from(".banner", { opacity: 0, duration: 1, delay: 1.1, x: 200 });
+gsap.from(".banner", { opacity: 0, duration: 1, delay: 1.1, x: -200 });
 gsap.from(".hero h3", { opacity: 0, duration: 1, delay: 1.4, y: -50 });
-gsap.from(".hero h4", { opacity: 0, duration: 1, delay: 1.7, y: -45 });
-gsap.from(".hero a", { opacity: 0, duration: 1, delay: 2.1, y: 50 });
+gsap.from(".hero h1", { opacity: 0, duration: 1, delay: 1.7, y: -45 });
+gsap.from(".hero h4", { opacity: 0, duration: 1, delay: 2.1, y: -30 });
+gsap.from(".hero a", { opacity: 0, duration: 1, delay: 2.4, y: -10 });
+
 gsap.from(".nav-item", {
   opacity: 0,
   duration: 1,
@@ -129,6 +151,6 @@ gsap.from(".icons span", {
   opacity: 0,
   duration: 1,
   delay: 2.5,
-  y: -30,
+  x: -30,
   stagger: 0.2,
 });
